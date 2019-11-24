4924b46fa2c9e43745839a77d84591378875ff85
Michał Czarnecki <czarnecky@va.pl>
2019-11-24 14:41:29 +0100

    Simplify users migration

--- migrations/20191124123630_users.js
+++ migrations/20191124123630_users.js
@@ -2,22 +2,16 @@
 const tableName = "users";
 
 exports.up = function(knex, Promise) {
-	return knex.schema.hasTable(tableName).then(function (exists)  {
-		if (!exists) {
-			return knex.schema.createTable(tableName, function (table) {
-				table.increments("id").primary();
-				table.string("login").notNull();
-				table.string("email").notNull();
-				table.string("password").notNull();
-				table.datetime("joined").notNull().defaultTo(knex.fn.now());
+	return knex.schema.createTable(tableName, function (table) {
+		table.increments("id").primary();
+		table.string("login").notNull();
+		table.string("email").notNull();
+		table.string("password").notNull();
+		table.datetime("joined").notNull().defaultTo(knex.fn.now());
 
-				table.unique(["login"]);
-				table.unique(["email"]);
-			}).then((result) => {}).catch((err) => {});
-		} else {
-			return
-		}
-	})
+		table.unique(["login"]);
+		table.unique(["email"]);
+	});
 };
 
 exports.down = function(knex) {
