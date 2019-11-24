4b97e0058ccf7ed7a1c181bde60b0277800f8b31
Michał Czarnecki <czarnecky@va.pl>
2019-11-24 14:42:40 +0100

    Fix knex config

--- src/services/knex/index.js
+++ src/services/knex/index.js
@@ -3,7 +3,7 @@ const knex = require("knex");
 
 module.exports = knex({
 	client: config.databaseClient,
-	connection: config.database
+	connection: config.databaseConnection
 });
 
 // vim:noai:ts=4:sw=4
