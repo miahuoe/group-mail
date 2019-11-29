39a74ba10ef1f0da8f5c447ac38e26e020782925
Michał Czarnecki <czarnecky@va.pl>
2019-11-29 22:07:13 +0100

    Add token middleware

---
+++ src/middlewares/token.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const config = require("../../config");
+
+const authToken = (req, res, next) => {
+	try {
+		const token = req.headers.authorisation.split(" ")[1];
+		const decoded = jwt.verify(token, process.env.JWT_SECRET, {
+			maxAge: config.jwt.expiration
+		});
+		req.user = {
+			id: decoded.id
+		};
+		next();
+	} catch (e) {
+		res.sendStatus(401);
+	}
+};
+
+module.exports = authToken;
+
+// vim:noai:ts=4:sw=4
