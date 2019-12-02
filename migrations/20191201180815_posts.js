1d169270f014620be1737f51e6835e59081c11e4
Michał Czarnecki <czarnecky@va.pl>
2019-12-02 11:33:35 +0100

    Change posts column
    Add groupId and rename author to authorId

--- migrations/20191201180815_posts.js
+++ migrations/20191201180815_posts.js
@@ -4,7 +4,8 @@ const tableName = "posts";
 exports.up = function(knex) {
 	return knex.schema.createTable(tableName, function (table) {
 		table.increments("id").primary();
-		table.integer("author").unsigned().references("users.id").notNull();
+		table.integer("groupId").unsigned().references("mail_groups.id").notNull();
+		table.integer("authorId").unsigned().references("users.id").notNull();
 		table.string("body").notNull();
 		table.datetime("created").notNull().defaultTo(knex.fn.now());
 	});
