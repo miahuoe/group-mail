fc30697e73cdf7af917491ebf611d50e354f7138
Michał Czarnecki <czarnecky@va.pl>
2019-12-07 19:35:07 +0100

    Validate group creation data

--- src/api/groups/controller.js
+++ src/api/groups/controller.js
@@ -1,16 +1,30 @@
 const Group = require("./model");
 const User = require("../users/model");
-const { transaction } = require('objection');
+const Joi = require("joi");
+
+//const { transaction } = require("objection");
+
+const generatePassword = () => {
+	return Date.now().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+};
 
 const create = async (req, res, next) => {
+	const schema = Joi.object({
+		maillocal: Joi.string().alphanum().min(4).max(20).required(),
+		name: Joi.string().alphanum().min(10).max(50).required(),
+	});
+	const v = schema.validate(req.body);
+	if (v.error) {
+		res.status(400).json({error: v.error});
+		return;
+	}
 	const newGroup = {
 		adminId: req.user.id,
-		maillocal: req.body.maillocal,
-		mailpass: Math.random().toString(36).substring(2, 15), // TODO
-		name: req.body.name,
+		maillocal: v.value.maillocal,
+		mailpass: generatePassword(),
+		name: v.value.name,
 		description: req.body.description
 	};
-
 	try {
 		// TODO transaction?
 		const g = await Group.query().insert(newGroup);
@@ -21,22 +35,15 @@ const create = async (req, res, next) => {
 	} catch (err) {
 		if (err.code == "ER_DUP_ENTRY") {
 			let message = ""
-			if (err.sqlMessage.indexOf("mail_groups_emaillocal") != -1) {
+			if (err.sqlMessage.indexOf("maillocal") != -1) {
 				message = "Mail occupied"
-			} else if (err.sqlMessage.indexOf("mail_groups_name") != -1) {
+			} else if (err.sqlMessage.indexOf("name") != -1) {
 				message = "Name occupied"
-			} else {
-				// TODO
 			}
-			res.status(409).json({
-				//error: err,
-				message: message
-			});
+			res.status(409).json({message});
 		} else {
-			res.status(500).json({ // TODO 5xx
+			res.status(500).json({
 				message: "Other error :(",
-				// TODO When does it fail exactly?
-				error: err
 			});
 		}
 	}
@@ -65,4 +72,4 @@ module.exports = {
 	create, getUsersGroups, invite,
 };
 
-// vim: noai:ts=4:sw=4
+// vim:noai:ts=4:sw=4
