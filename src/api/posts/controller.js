43397453a696ccf8e42aea26a9f8ad320f438e7e
Michał Czarnecki <czarnecky@va.pl>
2019-12-07 19:53:00 +0100

    Cleanup and add basic author data to each post

--- src/api/posts/controller.js
+++ src/api/posts/controller.js
@@ -3,7 +3,6 @@ const Group = require("../groups/model");
 const Joi = require("joi");
 
 const addPost = async (req, res, next) => {
-	//console.log(req); // TODO {}
 	const newPost = {
 		groupId: req.groupId,
 		authorId: req.user.id,
@@ -13,7 +12,7 @@ const addPost = async (req, res, next) => {
 		const p = await Post.query().insert(newPost);
 		// TODO missing creation date
 		delete p.groupId;
-		res.status(200).json(p);
+		res.status(201).json(p);
 	} catch (e) {
 		res.status(400).json(e); // TODO error
 	}
@@ -24,18 +23,22 @@ const getPosts = async (req, res, next) => {
 		offset: Joi.number().integer().min(0).max(1000).default(0),
 		limit: Joi.number().integer().min(5).max(50).default(10),
 	});
-	const v = schema.validate({
-		limit: req.query.limit,
-		offset: req.query.offset,
-	});
-	// offset 0-1000,0
-	// limit 5-50,10
+	const v = schema.validate(req.query);
+	if (v.error) {
+		res.status(400).json({error: v.error.details[0].message});
+		return;
+	}
 	try {
 		const g = await Group.query().findById(req.groupId);
 		const p = await g.$relatedQuery("posts")
+			.select("id", "body", "created as date", "authorId")
 			.orderBy("created", "desc")
 			.limit(v.value.limit)
 			.offset(v.value.offset);
+		for (post of p) {
+			await post.$relatedQuery("author").select("id", "login", "email", "joined");
+			delete post.authorId;
+		}
 		res.status(200).json(p);
 	} catch (e) {
 		res.status(400).json(e); // TODO error
@@ -46,4 +49,4 @@ module.exports = {
 	addPost, getPosts
 };
 
-// vim: noai:ts=4:sw=4
+// vim:noai:ts=4:sw=4
