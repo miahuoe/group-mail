06d5b65a321e85b23f6e3917fc96ae3eec81191a
Michał Czarnecki <czarnecky@va.pl>
2019-12-12 17:53:00 +0100

    Extend getMail() with validation. Add getMessage()

--- src/api/mail/controller.js
+++ src/api/mail/controller.js
@@ -1,32 +1,76 @@
 const Group = require("../groups/model");
 const { getMailFromDirectory } = require("./model");
 const { connect } = require("../../services/imap");
-//const Joi = require("joi");
+const Joi = require("joi");
 
 // https://github.com/mscdex/node-imap
 
 const getMail = async (req, res, next) => {
+	// TODO limit, offset, search
+	const schema = Joi.object({
+		offset: Joi.number().integer().min(0).max(1000).default(0),
+		limit: Joi.number().integer().min(5).max(50).default(10),
+		directory: Joi.string().valid("INBOX", "Sent", "Outbox", "Spam", "Drafts"),
+	});
+	const v = schema.validate({
+		limit: req.query.limit,
+		offset: req.query.offset,
+		directory: req.params.directory,
+	});
+	if (v.error) {
+		res.status(400).json({error: v.error.details[0].message});
+		return;
+	}
 	try {
 		//const g = await Group.query().findById(req.groupId);
 		//const i = connect(g.maillocal, g.mailpass);
-		const i = connect(process.env.TEST_IMAP_USER, process.env.TEST_IMAP_PASS);
-		const m = getMailFromDirectory(i, req.params.directory, (total, next) => "1:"+next).then((mail) => {
-			res.status(200).json(mail);
-		}).catch((e) => {
-			res.status(500).json({
-				error: e
-			});
-		});
+		const user = process.env.TEST_IMAP_USER;
+		const pass = process.env.TEST_IMAP_PASS;
+		const conn = await connect(user, pass);
+		const mail = await getMailFromDirectory(conn, v.value.directory, "44"); // TODO offset, limit
+		conn.end();
+		res.status(200).json(mail);
 	} catch (e) {
 		console.log(e);
-		res.status(400).json({
-			error: e
-		});
+		res.status(400).json({error: e});
+	}
+}
+
+const getMessage = async (req, res, next) => {
+	// TODO limit, offset, search
+	const schema = Joi.object({
+		offset: Joi.number().integer().min(0).max(1000).default(0),
+		limit: Joi.number().integer().min(5).max(50).default(10),
+		directory: Joi.string().valid("INBOX", "Sent", "Outbox", "Spam", "Drafts"),
+		messageId: Joi.number().integer(),
+	});
+	const v = schema.validate({
+		limit: req.query.limit,
+		offset: req.query.offset,
+		directory: req.params.directory,
+		messageId: req.params.messageId,
+	});
+	if (v.error) {
+		res.status(400).json({error: v.error.details[0].message});
+		return;
+	}
+	try {
+		//const g = await Group.query().findById(req.groupId);
+		//const i = connect(g.maillocal, g.mailpass);
+		const user = process.env.TEST_IMAP_USER;
+		const pass = process.env.TEST_IMAP_PASS;
+		const conn = await connect(user, pass);
+		const mail = await getMailFromDirectory(conn, v.value.directory, v.value.messageId); // TODO offset, limit
+		conn.end();
+		res.status(200).json(mail);
+	} catch (e) {
+		console.log(e);
+		res.status(400).json({error: e});
 	}
 }
 
 module.exports = {
-	getMail
+	getMail, getMessage
 };
 
 // vim: noai:ts=4:sw=4
