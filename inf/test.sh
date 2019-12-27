6add9e4559fc75e4d14bb139b197876d070e40cb
Michał Czarnecki <czarnecky@va.pl>
2019-12-27 18:49:20 +0100

    Update test docker runner

--- inf/test.sh
+++ inf/test.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo rm -r db-data/*
-sed 's/npm\ start/npm\ test/g' docker-compose.yml > docker-test.yml
+sed 's/npm\ run-script\ watch/while\ true;do\ npm\ run-script\ test;\ sleep\ 30;\ done/g' docker-compose.yml > docker-test.yml
 docker-compose -f docker-test.yml up --remove-orphans
 rm docker-test.yml
