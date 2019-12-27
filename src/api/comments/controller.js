616680cec4c994e2b3e15a8b41b789c83bb50e0e
Michał Czarnecki <czarnecky@va.pl>
2019-12-27 18:50:05 +0100

    404 when fetching comments from non-existent post

--- src/api/comments/controller.js
+++ src/api/comments/controller.js
@@ -11,7 +11,7 @@ const addComment = async (req, res, next) => {
 	try {
 		const c = await Comment.query().insert(newComment);
 		// TODO missing creation date
-		res.status(200).json(c);
+		res.status(201).json(c);
 	} catch (e) {
 		res.status(400).json(e); // TODO error
 	}
@@ -22,21 +22,30 @@ const getComments = async (req, res, next) => {
 		offset: Joi.number().integer().min(0).max(1000).default(0),
 		limit: Joi.number().integer().min(5).max(50).default(10),
 	});
-	const v = schema.validate(req.query);
+	let v = schema.validate(req.query);
 	if (v.error) {
 		res.status(400).json({error: v.error.details[0].message});
 		return;
 	}
+	v = v.value;
 	try {
 		const g = await Group.query().findById(req.groupId);
+		if (!g) throw "g404";
 		const p = await g.$relatedQuery("posts").findById(req.postId);
+		if (!p) throw "p404";
 		const c = await p.$relatedQuery("comments")
 			.orderBy("created", "desc")
-			.limit(v.value.limit)
-			.offset(v.value.offset);
+			.limit(v.limit)
+			.offset(v.offset);
 		res.status(200).json(c);
 	} catch (e) {
-		res.status(400).json(e); // TODO error
+		if (e === "g404") {
+			res.status(404).json({error: "No such group"});
+		} else if (e === "p404") {
+			res.status(404).json({error: "No such post"});
+		} else {
+			res.status(400).json(e); // TODO error
+		}
 	}
 }
 
