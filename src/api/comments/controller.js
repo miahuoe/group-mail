e0d03b8e32ebc54de241ccd16f4d6c450d9123c3
Michał Czarnecki <czarnecky@va.pl>
2019-12-31 14:23:43 +0100

    Add validation to comments

--- src/api/comments/controller.js
+++ src/api/comments/controller.js
@@ -1,19 +1,45 @@
+const Post = require("../posts/model");
 const Comment = require("../comments/model");
 const Group = require("../groups/model");
 const Joi = require("joi");
+const HTTPError = require("../../lib/HTTPError");
+
+const belongsToGroup = async (uid, gid) => {
+	const g = await Group.query().findById(gid);
+	if (!g) {
+		throw new HTTPError(404, "No such group");
+	}
+	return g.$relatedQuery("users").select("id").where("id", uid);
+};
 
 const addComment = async (req, res, next) => {
-	const newComment = {
-		postId: req.postId,
-		authorId: req.user.id,
-		body: req.body.body,
-	};
 	try {
+		const u = await belongsToGroup(req.user.id, req.groupId);
+		if (!u) {
+			throw new HTTPError(401, "Not in group");
+		}
+		const p = await Post.query().findById(req.postId);
+		if (!p) {
+			throw new HTTPError(404, "No such post");
+		}
+		const schema = Joi.object({
+			body: Joi.string().required(),
+		});
+		let v = schema.validate(req.body);
+		if (v.error) {
+			throw new HTTPError(400, v.error.details[0].message);
+		}
+		v = v.value;
+		const newComment = {
+			postId: req.postId,
+			authorId: req.user.id,
+			body: v.body,
+		};
 		const c = await Comment.query().insert(newComment);
 		// TODO missing creation date
 		res.status(201).json(c);
-	} catch (e) {
-		res.status(400).json(e); // TODO error
+	} catch (err) {
+		next(err);
 	}
 }
 
@@ -30,22 +56,16 @@ const getComments = async (req, res, next) => {
 	v = v.value;
 	try {
 		const g = await Group.query().findById(req.groupId);
-		if (!g) throw "g404";
+		if (!g) throw new HTTPError(404, "No such group");
 		const p = await g.$relatedQuery("posts").findById(req.postId);
-		if (!p) throw "p404";
+		if (!p) throw new HTTPError(404, "No such post");
 		const c = await p.$relatedQuery("comments")
 			.orderBy("created", "desc")
 			.limit(v.limit)
 			.offset(v.offset);
 		res.status(200).json(c);
-	} catch (e) {
-		if (e === "g404") {
-			res.status(404).json({error: "No such group"});
-		} else if (e === "p404") {
-			res.status(404).json({error: "No such post"});
-		} else {
-			res.status(400).json(e); // TODO error
-		}
+	} catch (err) {
+		next(err);
 	}
 }
 
