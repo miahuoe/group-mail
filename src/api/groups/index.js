9292edd38bd585e90c857bdc09db266c4bfe072c
Michał Czarnecki <czarnecky@va.pl>
2019-12-31 12:28:23 +0100

    Hopefully fixed groups index

--- src/api/groups/index.js
+++ src/api/groups/index.js
@@ -1,5 +1,5 @@
 const { Router } = require("express")
-const router = Router()
+const router = Router({ mergeParams: true });
 const { create, getUsersGroups, invite, leave, kick, getMembers } = require("./controller")
 const posts = require("../posts")
 const mail = require("../mail")
@@ -15,13 +15,13 @@ router.get("/", token, getUsersGroups);
 
 router.post("/", token, create);
 
-router.post("/:groupId/users", withGroupId, invite);
+router.post("/:groupId/users", token, invite);
 
-router.post("/:groupId/leave", withGroupId, leave);
+router.post("/:groupId/leave", token, leave);
 
-router.delete("/:groupId/users/:userId", withGroupId, kick);
+router.delete("/:groupId/users/:userId", token, kick);
 
-router.get("/:groupId/users", withGroupId, getMembers);
+router.get("/:groupId/users", token, getMembers);
 
 
 router.use("/:groupId/posts", withGroupId, posts);
