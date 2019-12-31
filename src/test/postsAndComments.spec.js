425f5d415afe43e802bcee6426970866df7ef3e7
Michał Czarnecki <czarnecky@va.pl>
2019-12-31 18:40:36 +0100

    Rewrite tests for comments and posts

--- src/test/postsAndComments.spec.js
+++ src/test/postsAndComments.spec.js
@@ -7,235 +7,214 @@ const randomString = () => {
 	return Date.now().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 };
 
-const login = randomString();
-const password = randomString();
-const email = randomString();
-let token = "";
-const group = {
-	name: randomString(),
-	maillocal: randomString(),
-	description: randomString()
-};
-let groupId = 0;
-
 describe("posts & comments", () => {
-	it("GET  /api/groups/:id:/posts should 401 when not authorized", (done) => {
-		request(app)
-			.get(`/api/groups/1/posts`)
-			.expect(401, done);
+	let users = [];
+	let groups = [];
+	before(async (done) => {
+		for (let i = 0; i < 6; i++) {
+			let user = {
+				login: randomString()+i,
+				password: randomString()+i,
+				email: randomString()+i+"@testmail.com",
+			};
+			await request(app)
+				.post("/api/users/register")
+				.send(user)
+				.expect(201);
+			await request(app)
+				.post("/api/users/login")
+				.auth(user.login, user.password)
+				.expect((res) => {
+					res.body.should.have.property("token");
+					user.token = res.body.token;
+				})
+				.expect(200);
+			users.push(user);
+		}
+		for (let i = 0; i < 3; i++) {
+			let group = {
+				name: randomString()+i,
+				maillocal: randomString()+i,
+				description: "desc lol",
+			};
+			await request(app)
+				.post("/api/groups")
+				.set({ Authorization: "Token "+users[i].token })
+				.send(group)
+				.expect((res) => {
+					res.body.should.have.property("id");
+					group.id = res.body.id;
+				})
+				.expect(201);
+			groups.push(group);
+		}
+		done();
 	});
-	it("POST /api/groups/:id:/posts should 401 when not authorized", (done) => {
-		request(app)
-			.post(`/api/groups/1/posts`)
-			.expect(401, done);
-	});
-	it("GET  /api/groups/:id/posts/:id/comments should 401 when not authorized", (done) => {
-		request(app)
-			.get(`/api/groups/1/posts/1/comments`)
-			.expect(401, done);
-	});
-	it("POST /api/groups/:id/posts/:id/comments should 401 when not authorized", (done) => {
-		request(app)
-			.post(`/api/groups/1/posts/1/comments`)
-			.expect(401, done);
-	});
-	it("POST /api/users/register should create an user", (done) => {
-		const req = {
-			login: login,
-			password: password,
-			email: email+"@xd.com"
-		};
-		request(app)
-			.post("/api/users/register")
-			.send(req)
-			.expect(201, done);
-	});
-	it("should login", (done) => {
-		request(app)
-			.post("/api/users/login")
-			.auth(login, password)
-			.expect("Content-Type", "application/json; charset=utf-8")
-			.expect((res) => {
-				token = res.body.token;
-			})
-			.expect(200, done);
-	});
-	it("should create a new group", (done) => {
-		request(app)
-			.post("/api/groups")
-			.set({
-				Authorization: "Token "+token,
-				"Content-Type": "application/json",
-			})
-			.send(group)
-			.expect((res) => {
-				res.body.should.have.property("id");
-				groupId = res.body.id;
-			})
-			.expect(201, done);
-	});
-	// TODO existing but not joined to
-	it("should 404 for inexistent group when getting", (done) => {
-		request(app)
-			.get(`/api/groups/${groupId+2}/posts`)
-			.set({
-				Authorization: "Token "+token,
-			})
-			.expect(404, done);
-	});
-	it("should 401 when not authorized", (done) => {
-		request(app)
-			.post(`/api/groups/${groupId+2}/posts`)
-			.expect(401, done);
-	});
-	it("should 404 for inexistent group when posting", (done) => {
-		const post = {
-			body: "hello",
-		};
-		request(app)
-			.post(`/api/groups/${groupId+2}/posts`)
-			.set({
-				Authorization: "Token "+token,
-			})
-			.send(post)
-			.expect(404, done);
-	});
-	it("GET  /api/groups/:id/posts should get created group posts", (done) => {
-		request(app)
-			.get(`/api/groups/${groupId}/posts`)
-			.set({
-				Authorization: "Token "+token,
-			})
-			.expect((res) => {
-				res.body.should.be.an.array;
-				res.body.should.be.empty;
-			})
-			.expect(200, done);
-	});
-	it(`POST /api/groups/:id/posts should 400 post without body`, (done) => {
-		const p = {
-		};
-		request(app)
-			.post(`/api/groups/${groupId}/posts`)
-			.set({
-				Authorization: "Token "+token,
-			})
-			.send(p)
-			.expect(400)
-			.end(done);
-	});
-	let posts = [];
-	for (i = 0; i < 2; i++) {
-		it(`POST /api/groups/:id/posts should post a valid post`, (done) => {
-			const p = {
-				body: `post body ${i}`,
+	describe("/api/groups/:id/posts", () => {
+		it("GET should 401 when not authorized", (done) => {
+			request(app)
+				.get(`/api/groups/1/posts`)
+				.expect(401, done);
+		});
+		it("POST should 401 when not authorized", (done) => {
+			request(app)
+				.post(`/api/groups/1/posts`)
+				.expect(401, done);
+		});
+		it("POST should 404 for inexistent group", (done) => {
+			request(app)
+				.get(`/api/groups/${groups[1].id+10}/posts`)
+				.set({ Authorization: "Token "+users[0].token })
+				.expect(404)
+				.end(done);
+		});
+		it("POST should 404 for inexistent group when posting", (done) => {
+			const post = {
+				body: "hello",
 			};
 			request(app)
-				.post(`/api/groups/${groupId}/posts`)
-				.set({
-					Authorization: "Token "+token,
+				.post(`/api/groups/${groups[1].id+10}/posts`)
+				.set({ Authorization: "Token "+users[0].token })
+				.send(post)
+				.expect(404)
+				.end(done);
+		});
+		it("GET should get empty array", (done) => {
+			request(app)
+				.get(`/api/groups/${groups[0].id}/posts`)
+				.set({ Authorization: "Token "+users[0].token })
+				.expect((res) => {
+					res.body.should.be.an.array;
+					res.body.should.be.empty;
 				})
+				.expect(200)
+				.end(done);
+		});
+		it(`POST should 400 post without body`, (done) => {
+			const p = {};
+			request(app)
+				.post(`/api/groups/${groups[0].id}/posts`)
+				.set({ Authorization: "Token "+users[0].token })
+				.send(p)
+				.expect(400)
+				.end(done);
+		});
+		it(`POST should post a valid post`, (done) => {
+			const p = { body: "post body" };
+			request(app)
+				.post(`/api/groups/${groups[0].id}/posts`)
+				.set({ Authorization: "Token "+users[0].token })
 				.send(p)
 				.expect((res) => {
 					res.body.should.have.property("id");
 					res.body.should.have.property("body");
-					posts.push(parseInt(res.body.id));
 				})
-				.expect(201, done);
+				.expect(201)
+				.end(done);
 		});
-	}
-	it("GET  /api/groups/:id/posts/:id/comments should 404 for inexistent post when getting", (done) => {
-		request(app)
-			.get(`/api/groups/${groupId}/posts/${posts[1]+2}/comments`)
-			.set({
-				Authorization: "Token "+token,
-			})
-			.expect(404)
-			.end(done);
 	});
-	it("POST /api/groups/:id/posts/:id/comments should 400 for invalid comment", (done) => {
-		const c = {
-		};
-		request(app)
-			.post(`/api/groups/${groupId}/posts/${posts[1]}/comments`)
-			.set({
-				Authorization: "Token "+token,
-			})
-			.send(c)
-			.expect(400, done);
-	});
-	it("POST /api/groups/:id/posts/:id/comments should 404 for inexistent post", (done) => {
-		const c = {
-			body: "hello",
-		};
-		request(app)
-			.post(`/api/groups/${groupId}/posts/${posts[1]+2}/comments`)
-			.set({
-				Authorization: "Token "+token,
-			})
-			.send(c)
-			.expect(404)
-			.end(done);
-	});
-	it(`should get posts`, (done) => {
-		request(app)
-			.get(`/api/groups/${groupId}/posts`)
-			.set({
-				Authorization: "Token "+token,
-			})
-			.expect((res) => {
-				res.body.should.be.an.array;
-				res.body.should.not.be.empty;
-				res.body[0].should.have.property("id");
-				res.body[0].should.have.property("body");
-				res.body[0].should.have.property("author");
-
-				//TODO test ordering, limit and offset
-				//res.body[0].id.should.be.equal(posts[0]);
-				//res.body[1].id.should.be.equal(posts[1]);
-			})
-			.expect(200, done);
-	});
-	let comments = [];
-	for (i = 0; i < 2; i++) {
-		it("should add a comment", (done) => {
-			const c = {
-				body: "comment body",
-			};
+	describe("/api/groups/:id/posts/:id/comments", () => {
+		it("GET should 401 when not authorized", (done) => {
 			request(app)
-				.post(`/api/groups/${groupId}/posts/${posts[0]}/comments`)
-				.set({
-					Authorization: "Token "+token,
+				.get(`/api/groups/1/posts/1/comments`)
+				.expect(401, done);
+		});
+		it("POST should 401 when not authorized", (done) => {
+			request(app)
+				.post(`/api/groups/1/posts/1/comments`)
+				.expect(401, done);
+		});
+	});
+	describe("commenting and posting", () => {
+		it("should 404 for inexistent post", (done) => {
+			request(app)
+				.get(`/api/groups/${groups[0].id}/posts/1/comments`)
+				.set({ Authorization: "Token "+users[0].token })
+				.expect(404)
+				.end(done);
+		});
+		it("should post 2 valid posts and retrieve them", async (done) => {
+			let posts = [];
+			for (i = 0; i < 2; i++) {
+				const p = { body: `post body ${i}` };
+				await request(app)
+					.post(`/api/groups/${groups[1].id}/posts`)
+					.set({ Authorization: "Token "+users[1].token })
+					.send(p)
+					.expect(201)
+					.expect((res) => {
+						res.body.should.have.property("id");
+						res.body.should.have.property("body");
+						posts.push(parseInt(res.body.id));
+					});
+			}
+			request(app)
+				.get(`/api/groups/${groups[1].id}/posts`)
+				.set({ Authorization: "Token "+users[1].token })
+				.expect(200)
+				.expect((res) => {
+					res.body.should.be.an.array;
+					res.body.should.not.be.empty;
+					res.body.should.have.length(2);
+					res.body[0].should.have.property("id");
+					res.body[0].should.have.property("body");
+					res.body[0].should.have.property("author");
+					res.body[0].id.should.be.equal(posts[1]);
+					res.body[1].id.should.be.equal(posts[0]);
 				})
-				.send(c)
+				.end(done);
+		});
+		it("should 401 when getting posts from existing group, as non-member", (done) => {
+			request(app)
+				.get(`/api/groups/${groups[2].id}/posts`)
+				.set({ Authorization: "Token "+users[0].token })
+				.expect(401)
+				.end(done);
+		});
+		it("should post 2 valid comments and retrieve them", async (done) => {
+			let comments = [];
+			let postid = 0;
+			const p = { body: `post body ${i}` };
+			await request(app)
+				.post(`/api/groups/${groups[2].id}/posts`)
+				.set({ Authorization: "Token "+users[2].token })
+				.send(p)
+				.expect(201)
 				.expect((res) => {
 					res.body.should.have.property("id");
 					res.body.should.have.property("body");
-					res.body.should.have.property("authorId");
-					comments.push(parseInt(res.body.id));
-				})
-				.expect(201, done);
+					postid = parseInt(res.body.id);
+				});
+			for (i = 0; i < 2; i++) {
+				await request(app)
+					.post(`/api/groups/${groups[2].id}/posts/${postid}/comments`)
+					.set({ Authorization: "Token "+users[2].token })
+					.send({ body: `comment ${i}` })
+					.expect(201)
+					.expect((res) => {
+						res.body.should.have.property("id");
+						res.body.should.have.property("body");
+						comments.push(parseInt(res.body.id));
+					});
+			}
+			request(app)
+				.get(`/api/groups/${groups[2].id}/posts/${postid}/comments`)
+				.set({ Authorization: "Token "+users[2].token })
+				.query({ limit: 5, offset: 0 })
+				.expect(200)
+				.expect((res) => {
+					res.body.should.be.an.array;
+					res.body.should.not.be.empty;
+					res.body.should.have.length(2);
+					res.body[0].should.have.property("id");
+					res.body[0].should.have.property("body");
+					res.body[0].should.have.property("authorId"); // TODO
+					res.body[0].id.should.be.equal(comments[1]);
+					res.body[1].id.should.be.equal(comments[0]);
+				}).end(done);
 		});
-	}
-	it("should get comments", (done) => {
-		request(app)
-			.get(`/api/groups/${groupId}/posts/${posts[0]}/comments`)
-			.query({
-				limit: 5,
-				offset: 0
-			})
-			.set({
-				Authorization: "Token "+token,
-			})
-			.expect((res) => {
-				res.body.should.be.an.array;
-				res.body[0].should.have.property("id");
-				//TODO test ordering, limit and offset
-				//res.body[0].id.should.be.equal(comments[0]);
-				//res.body[1].id.should.be.equal(comments[1]);
-			})
-			.expect(200, done);
 	});
+	// TODO limit and offset
 })
 
 // vim: noai:ts=4:sw=4
