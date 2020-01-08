666bcadb03ebcb9f0d2865ad66507affa07e8219
Michał Czarnecki <czarnecky@va.pl>
2020-01-08 14:27:07 +0100

    Move scripts to Makefile

--- inf/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-u=$USER
-sudo chown $u:$u -R db-data
-rm -rf db-data/*
--- inf/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sed 's/npm\ run-script\ watch/while\ true;do\ npm\ run-script\ test;\ sleep\ 30;\ done/g' docker-compose.yml > docker-test.yml
-docker-compose -f docker-test.yml up --remove-orphans
-rm docker-test.yml
