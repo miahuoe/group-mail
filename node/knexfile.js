cf19573c29d73ebae4fde5cc83ff38f856937e5e
Michał Czarnecki <czarnecky@va.pl>
2020-01-12 18:10:36 +0100

    Add test environment in knexfile

--- node/knexfile.js
+++ node/knexfile.js
@@ -1,12 +1,20 @@
 "use strict";
+/* jshint esversion: 8, node: true, unused: true */
 
 const config = require("./src/config");
 
 module.exports = {
-  development: {
-    client: config.databaseClient,
-    connection: config.databaseConnection
-  }
+	test: {
+		client: "sqlite3",
+		connection: {
+			filename: ":memory:",
+		},
+		useNullAsDefault: true
+	},
+	development: {
+		client: config.databaseClient,
+		connection: config.databaseConnection
+	}
 };
 
 // vim:noai:ts=4:sw=4
