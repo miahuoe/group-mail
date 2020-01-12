ee3c879eb4f84b8e294e35764a1368bf2d562311
Michał Czarnecki <czarnecky@va.pl>
2020-01-12 18:11:03 +0100

    Change initial autoincrement number for sqlite

--- node/migrations/20191201130227_groups.js
+++ node/migrations/20191201130227_groups.js
@@ -1,8 +1,11 @@
+"use strict";
+/* jshint esversion: 8, node: true, unused: true */
 
+const config = require("../src/config");
 const tableName = "mail_groups";
 
 exports.up = function(knex) {
-	return knex.schema.createTable(tableName, function (table) {
+	const k = knex.schema.createTable(tableName, function (table) {
 		table.increments("id").primary();
 		table.integer("adminId").unsigned().references("users.id").notNull();
 		table.string("name").notNull();
@@ -15,7 +18,14 @@ exports.up = function(knex) {
 
 		table.unique(["maillocal"]);
 		table.unique(["name"]);
-	}).raw(`ALTER TABLE ${tableName} AUTO_INCREMENT = 10000`); // NOTE dovecot accepts only >500
+	});
+	// NOTE dovecot accepts only >500
+	if (config.databaseClient === "sqlite3") {
+		k.raw(`UPDATE sqlite_sequence SET seq = 10000 WHERE name = '${tableName}'`);
+	} else if (config.databaseClient === "mysql") {
+		k.raw(`ALTER TABLE ${tableName} AUTO_INCREMENT = 10000`);
+	}
+	return k;
 };
 
 exports.down = function(knex) {
